25c4fea0e0a868c27192c0b558e05a82baade488
Justin Chan <justin.the.c@gmail.com>
2015-12-31 02:35:54 -0500

    Added options popup panel and cleaned up some code

--- redditnav.js
+++ redditnav.js
@@ -4,50 +4,6 @@
 UP = 0
 DOWN = 1
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTab(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    callback(tab);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTab(function(tab) {
-    chrome.tabs.executeScript(null, {
-        file: "redditnav.js"
-    });
-  });
-});
-
-
 function goToNextParent(pos, direction) {
   var parentComments = $(".sitetable.nestedlisting").children(".comment").toArray();
   parentComments = parentComments.map(function(commentElement){
@@ -155,4 +111,3 @@ $(function() {
 	}
   });
 });
-
