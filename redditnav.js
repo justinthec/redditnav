4801bb1a664ca30fbde090aa6e478f67ed5ded8f
Roland Pangu <faruzzy@gmail.com>
2016-06-30 14:41:35 -0700

    bumped z-index up

--- redditnav.js
+++ redditnav.js
@@ -90,7 +90,7 @@ $(function() {
     buttonPos: 'right'
   }, function(items) {
     $("head").append('<link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">');
-    var $floatingButton = $('<ul class="mfb-component--br mfb-slidein-spring" data-mfb-toggle="hover">\
+    var $floatingButton = $('<ul class="mfb-component--br mfb-slidein-spring" data-mfb-toggle="hover" style="z-index:999">\
       <li class="mfb-component__wrap">\
       <a id="redditNavDown" data-mfb-label="Next Thread (W)" class="mfb-component__button--main">\
         <i class="mfb-component__main-icon--resting ion-compass"></i>\
