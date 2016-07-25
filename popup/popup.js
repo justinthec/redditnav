dd5864954845494ddb10d8ffa71b622b672406c9
Justin Chan <justin.the.c@gmail.com>
2016-07-25 01:15:17 -0400

    Patch to clean up dcrousso's PR

--- popup/popup.js
+++ popup/popup.js
@@ -1,47 +1,47 @@
-var toastActive = false;
+let toastActive = false;
 function triggerToast () {
   if (toastActive)
     return;
 
   toastActive = true;
-  Materialize.toast('Refresh to see changes!', 4000, '', function() {
-    toastActive = false
+  Materialize.toast('Refresh to see changes!', 4000, '', () => {
+    toastActive = false;
   });
 }
 
-Array.from(document.querySelectorAll("button.color")).forEach(function(button) {
-  button.addEventListener("click", function(event) {
+Array.from(document.querySelectorAll('button.color')).forEach((button) => {
+  button.addEventListener('click', (event) => {
     chrome.storage.sync.set({
       color: window.getComputedStyle(button).backgroundColor
-    }, function() {
+    }, () => {
       triggerToast();
     });
   });
 });
 
-Array.from(document.querySelectorAll("input[type=radio]")).forEach(function(input) {
-  input.addEventListener("change", function(event) {
+Array.from(document.querySelectorAll('input[type=radio]')).forEach((input) => {
+  input.addEventListener('change', (event) => {
     if (!input.checked)
       return;
 
     chrome.storage.sync.set({
       buttonPos: input.value
-    }, function() {
+    }, () => {
       triggerToast();
     });
   });
 });
 
-document.querySelector("a[href=\"#ButtonTab\"]").addEventListener("click", function(event) {
+document.querySelector('a[href="#ButtonTab"]').addEventListener('click', (event) => {
   chrome.storage.sync.get({
     buttonPos: 'right'
-  }, function(items) {
+  }, (items) => {
     document.querySelector(`#${items.buttonPos}`).checked = true;
   });
 });
 
-Array.from(document.querySelectorAll("a.js-author-link")).forEach(function(link) {
-  link.addEventListener("click", function(event) {
+Array.from(document.querySelectorAll('a.js-author-link')).forEach((link) => {
+  link.addEventListener('click', (event) => {
     chrome.tabs.create({url: link.href});
     return false;
   });
